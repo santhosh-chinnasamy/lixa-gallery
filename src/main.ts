44e7d834f8bd98488afa7f3f6c25bfb4eb27366f
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2025-05-02 23:06:13 +0530

    select system folder

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import { invoke } from "@tauri-apps/api/core";
-
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
-
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
+import { open } from "@tauri-apps/plugin-dialog";
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
+  const folderSelector: HTMLButtonElement | null =
+    document.querySelector("#folder-selector-button");
+
+  folderSelector?.addEventListener("click", async () => {
+    const folder = await open({
+      multiple: false,
+      directory: true,
+      filters: []
+    });
+
+    const greetMsgEl: HTMLElement | null = document.querySelector("#greet-msg");
+    if (!greetMsgEl) return;
+
+    greetMsgEl.textContent = await invoke("greet", {
+      name: folder,
+    });
   });
 });
