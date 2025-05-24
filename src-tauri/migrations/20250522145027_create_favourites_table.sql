fdc4b05a85874eb0e55966184955201d64de65e9
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2025-05-24 16:16:52 +0530

    refactor: remove comment and add index on path column

--- src-tauri/migrations/20250522145027_create_favourites_table.sql
+++ src-tauri/migrations/20250522145027_create_favourites_table.sql
@@ -1,8 +1,6 @@
--- BEGIN;
 CREATE TABLE IF NOT EXISTS favourites (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     path TEXT NOT NULL
 );
 
--- CREATE INDEX IF NOT EXISTS ix_favourites_path ON favourites (path);
--- COMMIT;
+CREATE INDEX IF NOT EXISTS ix_favourites_path ON favourites (path);
