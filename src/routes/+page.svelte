a800b1b1034712e4b2837cc8839f600b7cb990c0
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2025-06-19 19:49:40 +0530

    feat: add app layout components and refactor pages
    refactor: move logic to shared components and operations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,95 +1,35 @@
 <script lang="ts">
   import { Button } from '$lib/components/ui/button';
-  import { invoke } from '@tauri-apps/api/core';
-  import { getCurrentWindow } from '@tauri-apps/api/window';
-  import { open } from '@tauri-apps/plugin-dialog';
+  import AppLayout from '../components/layout/AppLayout.svelte';
   import Gallery from '../components/Gallery.svelte';
+  import KeyboardShortcuts from '../components/common/KeyboardShortcuts.svelte';
+  import { loadPhotos } from '../components/common/ImageOperations';
   import { favorites, isLoading, photos } from '../stores/galleryStore';
-  import type { KeyboardActions } from '../types/events';
 
-  const APP_NAME = 'Lixa Gallery';
-
-  const loadPhotos = async () => {
-    try {
-      isLoading.set(true);
-      const folder = await open({
-        multiple: false,
-        directory: true,
-      });
-      if (!folder) {
-        isLoading.set(false);
-        photos.set([]);
-        return;
-      }
-      const loadedPhotos: string[] = await invoke('scan_folder', {
-        path: folder,
-      });
-      photos.set(loadedPhotos);
-    } catch (error) {
-      console.error('Failed to load photos:', error);
-    } finally {
-      isLoading.set(false);
-    }
-  };
-
-  const toggleFullScreen = async () => {
-    const fullscreen = await getCurrentWindow().isFullscreen();
-
-    if (!fullscreen) {
-      await getCurrentWindow().setFullscreen(true);
-    }
-
-    if (fullscreen) {
-      await getCurrentWindow().setFullscreen(false);
-    }
-  };
-
-  const keyboardActions: KeyboardActions = {
+  const keyboardActions = {
     o: loadPhotos,
-    F11: toggleFullScreen,
-  };
-
-  const handleKeydown = (event: KeyboardEvent) => {
-    try {
-      const action = keyboardActions[event.key];
-      if (action) action();
-    } catch (error) {
-      console.error(
-        `Error executing keyboard action: [key: ${event.key}]`,
-        error,
-      );
-    }
   };
 </script>
 
-<svelte:window onkeydown={handleKeydown} />
+<KeyboardShortcuts actions={keyboardActions} />
 
-<main class="max-w-[100vw]">
-  <header
-    class="sticky top-0 z-10 flex items-center justify-between bg-gray-50 px-4 py-6 shadow-sm"
-  >
-    <h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">
-      {APP_NAME}
-    </h3>
-    <div>
-      <a
-        href="/favourites"
-        class="inline-flex h-10 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-      >
-        Show Favourites {$favorites.size}
-      </a>
+<AppLayout title="Lixa Gallery">
+  <svelte:fragment slot="header-actions">
+    <a
+      href="/favourites"
+      class="inline-flex h-10 items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
+    >
+      Show Favourites {$favorites.size}
+    </a>
+  </svelte:fragment>
+
+  {#if $isLoading}
+    <p class="text-center">Loading...</p>
+  {:else if $photos.length === 0}
+    <div class="flex justify-center items-center">
+      <Button on:click={loadPhotos}>Open Folder</Button>
     </div>
-  </header>
-
-  <div class="top-10 flex items-center justify-center p-4">
-    {#if $isLoading}
-      <p class="text-center">Loading...</p>
-    {:else if $photos.length === 0}
-      <div>
-        <Button on:click={loadPhotos}>Open Folder</Button>
-      </div>
-    {:else}
-      <Gallery photos={$photos} />
-    {/if}
-  </div>
-</main>
+  {:else}
+    <Gallery photos={$photos} />
+  {/if}
+</AppLayout>
