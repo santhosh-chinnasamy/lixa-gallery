0b7302866eed5d29e18559d0ed28f8c34bff5e85
Santhosh Chinnasamy <csesanthosh15@gmail.com>
2025-08-23 12:13:29 +0530

    add migration for photos table

---
+++ src-tauri/migrations/20250813155433_create_photos_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE
+    IF NOT EXISTS photos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        path TEXT NOT NULL,
+        name TEXT NOT NULL,
+        thumbnail_path TEXT NOT NULL,
+        mtime INTEGER NOT NULL, -- file modification time
+        ctime INTEGER NOT NULL, -- file creation time
+        size INTEGER NOT NULL, -- file size in bytes
+        created_at INTEGER DEFAULT (unixepoch ()) -- time when the photo was added to Lixa
+    );
+
+CREATE INDEX IF NOT EXISTS idx_photos_path ON photos (path);
+
+CREATE INDEX IF NOT EXISTS idx_photos_size ON photos (size);
+
+CREATE INDEX IF NOT EXISTS idx_photos_mtime ON photos (mtime);
+
+CREATE INDEX IF NOT EXISTS idx_photos_created_at ON photos (created_at);
